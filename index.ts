4a7a885ae49b0f22dfe737a4eabeefff14c64f30
samuel <samzyfaps@gmail.com>
2024-02-04 15:32:30 +0100

    added new functions

--- index.ts
+++ index.ts
@@ -2,17 +2,12 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from "cors";
 import passport from 'passport';
-// import cookieSession from "cookie-session";
 import session from 'express-session';
-// import { config as dotenvConfig } from "dotenv";
 
 import authRouter from "./routes/auth";
 import urlChecksRouter from "./routes/urlCheck";
-import config from "./config/config.secrets";
 import reportsRouter from "./routes/reports";
-import profileRouter from "./routes/profile";
 import eventEmitter from "./controllers/events";
-// import connectDB from "./Config/config.db";
 import errorHandler from "./middlewares/errorHandler";
 import authorize from "./middlewares/authorize";
 import "./passport"
@@ -30,16 +25,6 @@ app.get("/", (req, res) => {
   res.render("home", { user: req.user });
 });
 
-
-
-
-// setting up cookieSession
-// app.use(
-//   cookieSession({
-//     maxAge: 24 * 60 * 60 * 1000,
-//     keys: [config.COOKIE_KEY],
-//   })
-// );
 
 app.use(session({
   secret: 'your_secret_key',
@@ -63,7 +48,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/api/v1/auth", authRouter);
-app.use("/profile", profileRouter);
 app.use("/api/v1/checks/", authorize, urlChecksRouter);
 app.use("/api/v1/reports/", authorize, reportsRouter);
 
